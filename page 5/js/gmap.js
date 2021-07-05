57b441be12813072c0ecb78c7c7a07bdad873b6c
Jahangir-Alam-Noman <jahangir2km@gmail>
2021-07-05 16:19:10 +0600

    otodi added

---
+++ page 5/js/gmap.js
@@ -0,0 +1,10 @@
+﻿$("#map").gmap3({
+        marker:{
+            address: "R. I Tower, 4th Floor,Golpahar, মোহাম্মদ আলী সড়ক, Chittagong, Bangladesh"
+        },
+    map:{
+            options:{
+                    zoom: 20
+                }
+        }
+});
---
+++ page3/js/gmap.js
@@ -0,0 +1,10 @@
+﻿$("#map").gmap3({
+        marker:{
+            address: "23.731072,90.402884"
+        },
+    map:{
+            options:{
+                    zoom: 15
+                }
+        }
+});
